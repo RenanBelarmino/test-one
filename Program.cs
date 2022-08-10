4365ebd8c05d70379b77320276ce49f9cc22f260
RenanBelarmino <69680055+RenanBelarmino@users.noreply.github.com>
2022-08-10 15:55:17 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Windows.Forms;
+
+namespace fluxodecaixa
+{
+    static class Program
+    {
+        /// <summary>
+        /// Ponto de entrada principal para o aplicativo.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            using (var db = new DatabaseContexto())
+            {
+                db.Database.Migrate();
+            }
+
+            Application.Run(new FormLogin());
+        }
+    }
+}
